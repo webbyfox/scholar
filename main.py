5618ece619a10d41dc9c9aca673713ec99aeda17
Rizwan Mansuri <mr.mansuri@gmail.com>
2017-05-03 21:21:06 +0100

    latest update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import scholar
 import csv
+import subprocess
 
 def read_data():
 	with open ('Articles.csv' , 'rb') as csvfile:
@@ -9,9 +10,25 @@ def read_data():
 			counter += 1;
 			if counter != 1:
 				article_title,article_type,article_author = row[1], row[2], row[3]
-				print article_title, article_type, article_author
-			
-			if counter > 2:
+				# print article_title, article_type, article_author
+				cmd = 'python scholar.py -c 1 --author ' + article_author + '--title ' + article_title
+				p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+				out, err = p.communicate() 
+				result = out.split('\n')
+				for lin in result:
+					for item in ['Title', 'URL','Citations']:
+						if lin.find(item) != -1 and not lin.startswith('#'):
+							# print(lin.strip())
+				
+							with open('result.csv', 'w') as csvfile:
+							    fieldnames = ['Article', 'URL', 'Citation']
+							    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+							    writer.writeheader()
+							    writer.writerow({'Article': lin.strip(), 'URL': lin.strip(), 'Citation': lin.strip()})
+							   
+
+			if counter > 1:
 				break;
 
 if __name__ == "__main__":
